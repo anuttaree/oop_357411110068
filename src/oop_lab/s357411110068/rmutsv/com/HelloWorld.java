79b4bcb23b0014cc0f4dd09901d1b03492f237a8
anuttaree <anuttaree068@gmail.com>
2017-11-19 15:20:00 +0700

    55

--- src/oop_lab/s357411110068/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s357411110068/rmutsv/com/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
 public static void main(String[] args) {
 
     System.out.println("Hello World, I'am ANN");
+    System.out.println("Anuttaree Srisaart");
+    System.out.println("Information System");
+    System.out.println("MT RMUTSV");
 
 
 
