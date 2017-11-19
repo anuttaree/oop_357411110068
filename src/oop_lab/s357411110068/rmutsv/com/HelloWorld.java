c12f5ae73c61bb6da6245bfa309f236baa8adf35
anuttaree <anuttaree068@gmail.com>
2017-11-19 15:00:24 +0700

    Initial commit

---
+++ src/oop_lab/s357411110068/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s357411110068.rmutsv.com;
+
+public class HelloWorld {
+
+//   create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I'am ANN");
+
+
+
+}// main
+
+}// class
+
