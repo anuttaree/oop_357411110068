8babbe90d2b2a31250e183a08bfcc7826c489666
anuttaree <anuttaree068@gmail.com>
2017-12-12 09:13:21 +0700

    Cal Grade

--- src/oop_lab3/CalGrade.java
+++ src/oop_lab3/CalGrade.java
@@ -1,5 +1,7 @@
 package oop_lab3;
 
+import java.util.Scanner;
+
 public class CalGrade {
     // score 0-49 grade F
     // score 50-59 grade D
@@ -7,4 +9,20 @@ public class CalGrade {
     // score 70-79 grade B
     // score 80-100 grade A
 
-}
+
+    public static void main(String[] args) {
+        //1. create objict for get input from user.
+        Scanner scanner=new Scanner(System.in);
+        //2. declare vrible for store data.
+        int score;
+        System.out.println("Enter your score: ");
+        score= scanner.nextInt();
+        //3. test varible with condition above.
+        //4. show the output on screen.
+        if (score<=49)System.out.println("Your grade is F");
+        else if (score<=59)System.out.println("Your grade is D");
+        else if (score<=69)System.out.println("Your grade is C");
+        else if (score<=79)System.out.println("Your grade is B");
+        else System.out.println("Your grade is A");
+    }//main
+}//class
