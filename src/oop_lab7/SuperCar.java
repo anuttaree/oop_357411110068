119d43f2dda1f2fd8479aa4e31ca6160bb722485
anuttaree <anuttaree068@gmail.com>
2018-01-23 10:51:21 +0700

    Class and Object: SuperCar Class

---
+++ src/oop_lab7/SuperCar.java
@@ -0,0 +1,78 @@
+package oop_lab7;
+
+//car brand
+//car cllor
+//car engine size
+//max speed
+//country of origin
+
+public class SuperCar {
+    @Override
+    public String toString() {
+        return "SuperCar{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", enginesize='" + enginesize + '\'' +
+                ", maxspeed='" + maxspeed + '\'' +
+                ", origin='" + origin + '\'' +
+                '}';
+    }
+
+    private String brand;
+    private String color;
+    private String enginesize;
+    private String maxspeed;
+    private String origin;
+    //Constructor
+    //Default constructor
+    public SuperCar(){}
+    public SuperCar(String b,String c,String e,String m,String o){
+        //assign data to class properties
+        this.brand = b;
+        this.color = c;
+        this.enginesize = e;
+        this.maxspeed = m;
+        this.origin = o;
+    }
+    //getter and setter method
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getEnginesize() {
+        return enginesize;
+    }
+
+    public void setEnginesize(String enginesize) {
+        this.enginesize = enginesize;
+    }
+
+    public String getMaxspeed() {
+        return maxspeed;
+    }
+
+    public void setMaxspeed(String maxspeed) {
+        this.maxspeed = maxspeed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}//class
---
+++ src/oop_lab7/mySuperCar.java
@@ -0,0 +1,34 @@
+package oop_lab7;
+
+import java.util.Scanner;
+
+public class mySuperCar {
+
+
+
+    public static void main (String [] args) {
+        //create Objects as SuperCar
+        SuperCar supercar1 = new SuperCar();
+
+        supercar1 = inputData(supercar1);
+
+        System.out.println(supercar1.toString());
+    }//main
+    private static SuperCar inputData(SuperCar s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your supercar data: ");
+        System.out.print("Brand: ");
+        s.setBrand(scanner.nextLine());
+        System.out.print("color: ");
+        s.setColor(scanner.nextLine());
+        System.out.print("enginesize: ");
+        s.setEnginesize(scanner.nextLine());
+        System.out.print("maxspeed: ");
+        s.setMaxspeed (scanner.nextLine());
+        System.out.print("origin: ");
+        s.setOrigin(scanner.nextLine());
+
+
+        return s;
+    }
+}//class
