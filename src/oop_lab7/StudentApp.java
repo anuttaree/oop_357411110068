0ae08cb9af5743a2f78dcb335f4be747a9e4fc97
anuttaree <anuttaree068@gmail.com>
2018-01-23 09:17:23 +0700

    Class and Object: Student Class

---
+++ src/oop_lab7/StudentApp.java
@@ -0,0 +1,12 @@
+package oop_lab7;
+
+public class StudentApp {
+    public static void main (String [] args){
+        //create Objects as Student
+        Student student1 =new Student();
+
+
+
+
+    }//main
+}//class
